f6aa38bb6995cbadb65abe7dfe4e932e4e9c1b13
Leidejane <leidejanedarosa.81@gmail.com>
2025-11-05 15:23:02 -0300

    feat: create main app component structure

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Services from './components/Services'
+
+function App() {
+  return (
+    <div className='min-h-screen bg-gray-50'>
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
