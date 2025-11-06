d5488b6515675507fb374c1b79754e3445857229
Leidejane <leidejanedarosa.81@gmail.com>
2025-11-06 18:45:28 -0300

    feat: integrate SEO component in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Contact from './components/Contact'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import SEO from './components/SEO'
 import Services from './components/Services'
 
 function App() {
   return (
     <div className='min-h-screen bg-gray-50'>
+      <SEO />
       <Header />
       <main>
         <Hero />
@@ -21,4 +23,3 @@ function App() {
 }
 
 export default App
-// Test comment
