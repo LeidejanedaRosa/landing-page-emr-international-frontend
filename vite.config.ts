81742e59fd8c6bbb654ebfb8e8410dafbe51001f
Leidejane <leidejanedarosa.81@gmail.com>
2025-11-11 19:27:06 -0300

    build: enhance Vite configuration with asset optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,36 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     open: true,
-    host: true, // Permite acesso externo
+    host: true,
   },
   build: {
     outDir: 'dist',
     sourcemap: true,
-    target: 'es2022', // Suporte a recursos modernos do JavaScript
+    target: 'es2022',
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
         },
+        assetFileNames: assetInfo => {
+          if (!assetInfo.name) return `assets/[name]-[hash][extname]`
+          const info = assetInfo.name.split('.')
+          const extType = info[info.length - 1]
+
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico|webp|avif/i.test(extType)) {
+            return `images/[name]-[hash][extname]`
+          }
+          return `assets/[name]-[hash][extname]`
+        },
       },
     },
+    assetsInlineLimit: 0,
+    cssCodeSplit: true,
   },
   preview: {
     port: 3000,
