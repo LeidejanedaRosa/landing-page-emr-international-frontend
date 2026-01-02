e118bc2bc256854b283e84024f0f114565987261
Leidejane <leidejanedarosa.81@gmail.com>
2026-01-02 16:14:27 -0300

    refactor: alphabetize Hero component exports

--- src/components/sections/Hero/components/index.ts
+++ src/components/sections/Hero/components/index.ts
@@ -1,11 +1,5 @@
-/**
- * Hero Components Barrel Export
- *
- * Centralized exports for better organization
- */
-
-export { TrustBadge } from './TrustBadge'
-export { HeroHeadline } from './HeroHeadline'
 export { HeroCTA } from './HeroCTA'
-export { SocialProof } from './SocialProof'
+export { HeroHeadline } from './HeroHeadline'
 export { HeroVisual } from './HeroVisual'
+export { SocialProof } from './SocialProof'
+export { TrustBadge } from './TrustBadge'
