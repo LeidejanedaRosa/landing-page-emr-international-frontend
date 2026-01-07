8b2a79581cc11327c176988f40fea4b16b9c3b27
Leidejane <leidejanedarosa.81@gmail.com>
2026-01-07 18:10:52 -0300

    fix: adjust viewport height to account for header and promo banner

--- src/components/sections/HeroCarousel/components/CarouselContainer.tsx
+++ src/components/sections/HeroCarousel/components/CarouselContainer.tsx
@@ -43,7 +43,7 @@ export const CarouselContainer = ({
       aria-label={HERO_CAROUSEL_A11Y.ariaLabel}
     >
       <div
-        className='relative min-h-screen h-[150vh] lg:h-[calc(100svh-150px)]'
+        className='relative h-screen md:h-[calc(100svh-150px)]'
         onMouseEnter={onPause}
         onMouseLeave={onResume}
         onFocus={onPause}
--- src/components/sections/HeroCarousel/index.tsx
+++ src/components/sections/HeroCarousel/index.tsx
@@ -65,7 +65,7 @@ const HeroCarousel = () => {
 
   if (!hasOpenEnrollment) {
     return (
-      <div className='h-[100svh] xl:h-[calc(100svh - 150px)]'>
+      <div className='h-screen md:h-[calc(100svh-130px)]'>
         <Hero />
       </div>
     )
